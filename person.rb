505ab1fe0daa1a078fc10a70101b2c6809596f32
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-23 00:02:20 +0100

    Added person class as required

---
+++ person.rb
@@ -0,0 +1,20 @@
+class Person
+  attr_accessor :age, name
+  attr_reader :id
+
+  def initialize(age, name = 'Unkown', parent_permission: true)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+  def can_use_service?
+    of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
